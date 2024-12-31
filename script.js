e5597e8192b059e517c74bc280d3dd9526e883e3
Alejandro Guerra <coreartalex2@gmail.com>
2024-12-30 18:39:56 -0800

    first js code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // https://jacintodesign.github.io/quotes-api/data/quotes.json
+let apiQuotes = [];
+
+// Shopw new quote
+function newQuote() {
+  // Random quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+// Get Quotes from API
+
+async function getQuotes(params) {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+    console.log(apiQuotes);
+  } catch (error) {
+    // Catch the error
+  }
+}
+
+// On Load
+getQuotes();
